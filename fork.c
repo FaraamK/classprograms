cf454da3b345c7da7d74323f6a9170cff95cd170
FaraamK <99051838+FaraamK@users.noreply.github.com>
2022-02-10 13:17:37 -0500

    Add files via upload

---
+++ fork.c
@@ -0,0 +1,29 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    pid_t pid;
+
+
+    pid = fork();
+    pid = fork();
+    pid = fork();
+
+
+    if (pid < 0) {
+        printf (stderr, "Fork Failed");
+        return 1;
+    }
+
+    else if (pid ==0) {
+        execlp("/bin/ls","ls", NULL);
+    }
+    else {
+        wait(NULL);
+        printf("Child Complete");
+    }
+printf(getpid());
+    return 0;
+}
