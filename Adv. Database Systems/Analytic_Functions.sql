53e2ac6f1139faa1e640ebc64dd586498d517890
Sean <99051838+FaraamK@users.noreply.github.com>
2022-04-08 11:08:59 -0400

    Update Analytic_Functions.sql

--- Adv. Database Systems/Analytic_Functions.sql
+++ Adv. Database Systems/Analytic_Functions.sql
@@ -1,5 +1,13 @@
 -- problem 1
 
+SELECT CDEPT, CNO, CNAME, CLABFEE, AVG(CLABFEE) 
+FROM COURSE 
+GROUP BY CDEPT, CNO, CNAME, CLABFEE
+ORDER BY CDEPT;
+
+/* code for problem one does present an output in the correct format, but the lab fee average does not print in the wanted way, 
+all CLABFEE should be totaled and averaged and should read 110 all the way down, work in progress */
+
 -- problem 2 
 
 SELECT CDEPT, CNO, CNAME, CLABFEE,
