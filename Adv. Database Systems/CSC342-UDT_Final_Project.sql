e37c043014d045418442ce1b0699c320192635b9
Sean <99051838+FaraamK@users.noreply.github.com>
2022-04-18 14:36:16 -0400

    Update CSC342-UDT_Final_Project.sql

--- Adv. Database Systems/CSC342-UDT_Final_Project.sql
+++ Adv. Database Systems/CSC342-UDT_Final_Project.sql
@@ -1,3 +1,20 @@
 /* work for CSC 342 Final Project will be stored here, currently, none has been done, 
 will update with time stamps and proper documentation for ease of understanding, 
 once each part has been done, I will label each as 'Part 1' 'Part 2' and 'Final' */ 
+
+-- part one work 
+
+CREATE OR REPLACE TYPE POINTS AS OBJECT 
+(LABEL  VARCHAR2(1),
+X       NUMBER(2),
+Y       NUMBER(2));
+
+
+DESCRIBE POINTS;
+
+CREATE TABLE POINTS_TABLE
+(LABEL  POINTS,
+X       POINTS,
+Y       POINTS);
+
+DESCRIBE POINTS_TABLE;
